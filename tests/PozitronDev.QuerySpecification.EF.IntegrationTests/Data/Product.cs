5274e1137fda8fe1a2669ab5053e6bde033d1bac
Fati Iseni <fati.iseni@pozitrongroup.com>
2020-07-18 23:11:59 +0200

    Enable nullable reference types.

--- tests/PozitronDev.QuerySpecification.EF.IntegrationTests/Data/Product.cs
+++ tests/PozitronDev.QuerySpecification.EF.IntegrationTests/Data/Product.cs
@@ -7,9 +7,9 @@ namespace PozitronDev.QuerySpecification.EF.IntegrationTests.Data
     public class Product
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string? Name { get; set; }
 
         public int StoreId { get; set; }
-        public Store Store { get; set; }
+        public Store? Store { get; set; }
     }
 }
