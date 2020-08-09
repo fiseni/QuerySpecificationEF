cc0969dcace92d3f818bd575a97f41a4e9d86c17
Fati Iseni <fati.iseni@pozitrongroup.com>
2020-08-09 20:29:09 +0200

    Adding ISearchEvaluator and updating evaluator.

--- src/PozitronDev.QuerySpecification.EF/SpecificationEvaluator.cs
+++ src/PozitronDev.QuerySpecification.EF/SpecificationEvaluator.cs
@@ -8,6 +8,18 @@ namespace PozitronDev.QuerySpecification.EF
 {
     public class SpecificationEvaluator<T> : SpecificationEvaluatorBase<T> where T : class
     {
+        private readonly ISearchEvaluator<T>? searchEvaluator;
+
+        public SpecificationEvaluator()
+        {
+            this.searchEvaluator = null;
+        }
+
+        public SpecificationEvaluator(ISearchEvaluator<T>? searchEvaluator)
+        {
+            this.searchEvaluator = searchEvaluator;
+        }
+
         public override IQueryable<TResult> GetQuery<TResult>(IQueryable<T> inputQuery, ISpecification<T, TResult> specification)
         {
             var query = GetQuery(inputQuery, (ISpecification<T>)specification);
@@ -30,6 +42,16 @@ namespace PozitronDev.QuerySpecification.EF
                 if (!string.IsNullOrEmpty(includeString))
                 {
                     query = query.Include(includeString);
+                }
+            }
+
+            foreach (var searchCriteria in specification.SearchCriterias)
+            {
+                var searchExpression = searchEvaluator?.GetExpression(searchCriteria.SearchTerm, searchCriteria.SearchType);
+
+                if (searchExpression != null)
+                {
+                    query = query.Where(searchExpression);
                 }
             }
 
